495dae51d49a1a187b8b198c9fda34a4fc1e2fd9
odera <eugeneodera59@gmail.com>
2024-09-30 22:43:26 +0300

    add text

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=function(drivers){
+    return drivers.slice(0,2)
+}
+ const drivers=['Antonia','Nuru','Amari','Mo']
+ console.log(returnFirstTwoDrivers(drivers))
+
+ const returnLastTwoDrivers=function(drivers){
+    return drivers.slice(-2)
+ }
+ console.log(returnLastTwoDrivers(drivers))
+ 
+ const selectingDrivers  = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+ const createFareMultiplier=function(Multiplier){
+    return function (fare){
+        return fare*Multiplier;
+    }
+ }
+ const fareDoubler= (createFareMultiplier(2)) 
+
+ const fareTripler= (createFareMultiplier(3))
+
+ function selectDifferentDrivers(drivers,driverchosen){
+    return driverchosen(drivers);
+
+ }
